68fe7656aafa107feabd67300e804dabe6ae4907
Amarildo <kondirildi@yahoo.com>
2020-10-20 19:56:30 +0200

    adding chages

--- src/main/java/com/akondi/GaussTask/model/EquationSystem.java
+++ src/main/java/com/akondi/GaussTask/model/EquationSystem.java
@@ -2,8 +2,6 @@ package com.akondi.GaussTask.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
